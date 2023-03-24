7c10722b9a3dfc49fc5b167ea6cdf20943c4ef54
rwudboy <ganznabil620@gmail.com>
2023-03-24 09:09:34 +0700

    Penjumlahan dengan methode jumlah tiga angka

---
+++ ConsoleApp1/Penjumlahan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace mod5_1302210047
+{
+    public class Penjumlahan
+    {
+            private dynamic data;
+            public Penjumlahan() { }
+            public T PenjumlahanTigaAngka<T>(T a, T b, T c)
+            {
+                dynamic x = a;
+                dynamic y = b;
+                dynamic z = c;
+
+                data = x + y + z;
+            }
+
+        public void Print()
+        {
+            Console.WriteLine($"Hasil Penjumlahan: {this.data}");
+        }
+    }
+
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace mod5_1302210047
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Penjumlahan Penjumlahan = new Penjumlahan();
+            Penjumlahan.PenjumlahanTigaAngka<float>(13, 02, 21);
+            Penjumlahan.Print();
+        }
+    }
+}
